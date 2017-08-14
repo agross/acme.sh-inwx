8b9ed7bbf39f1c53196b3d88aa363eb9555f198c
Jonas Gröger <jonas@huntun.de>
2017-08-15 01:52:25 +0200

    quote path

--- dns_inwx.sh
+++ dns_inwx.sh
@@ -5,10 +5,10 @@ BASEDIR=$(dirname "$(readlink -f "$0")")
 dns_inwx_add() {
     fulldomain=$1
     txtvalue=$2
-    $BASEDIR/acme-inwx.py --add --domain "$fulldomain" --challenge "$txtvalue"
+    ${BASEDIR}/acme-inwx.py --add --domain "$fulldomain" --challenge "$txtvalue"
 }
 
 dns_inwx_rm() {
     fulldomain=$1
-    $BASEDIR/acme-inwx.py --remove --domain "$fulldomain"
+    ${BASEDIR}/acme-inwx.py --remove --domain "$fulldomain"
 }
