b4b3601a027d0bf995a950130ae852ae626508d6
Jonas Gröger <jonas@huntun.de>
2017-08-15 02:26:02 +0200

    Fix parameter

--- dns_inwx.sh
+++ dns_inwx.sh
@@ -5,10 +5,10 @@ BASEDIR="ABSOLUTE_PATH_TO_THIS_PROJECT_FOLDER"
 dns_inwx_add() {
     fulldomain=$1
     txtvalue=$2
-    ${BASEDIR}/acme-inwx.py --add --domain "$fulldomain" --challenge "$txtvalue"
+    ${BASEDIR}/acme-inwx.py --add --acme-record-name "$fulldomain" --challenge "$txtvalue"
 }
 
 dns_inwx_rm() {
     fulldomain=$1
-    ${BASEDIR}/acme-inwx.py --remove --domain "$fulldomain"
+    ${BASEDIR}/acme-inwx.py --remove --acme-record-name "$fulldomain"
 }
